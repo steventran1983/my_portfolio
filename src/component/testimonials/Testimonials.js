ee257b75a30c391ad11a309dfe743b339f9e5efe
ttran65 <thang.tran65@t-mobile.com>
2022-10-16 00:09:13 -0500

    done testimonials

--- src/component/testimonials/Testimonials.js
+++ src/component/testimonials/Testimonials.js
@@ -1,11 +1,55 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
 
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+// import required modules
+import { Pagination } from "swiper";
+
+import { Data } from "./testimonialsData";
+import "./testimonials.css";
 const Testimonials = () => {
 	return (
-		<div className="testinomials section" id="testimonials">
-			<h2 className="section__title">Testimonials</h2>
-			<span className="section__subtitle">People saying about me !</span>
-			<div className="testimonials__conainer container"></div>
+		<div className="testinomial container section" id="testimonials">
+			<h2 className="section__title">People saying about me !</h2>
+			<span className="section__subtitle">Testimonials</span>
+			<div className="testimonial__conainer container"></div>
+			<Swiper
+				loop={true}
+				grabCursor={true}
+				spaceBetween={24}
+				pagination={{
+					clickable: true,
+				}}
+				breakpoints={{
+					576: {
+						slidesPerView: 2,
+					},
+					768: {
+						slidesPerView: 2,
+						spaceBetween: 48,
+					},
+				}}
+				modules={[Pagination]}
+				className="testimonial__container"
+			>
+				{Data.map(({ id, image, title, description }) => (
+					<SwiperSlide className="testimoniial__card" key={id}>
+						<img
+							src={image}
+							alt="photo"
+							className="testimonial__img"
+						/>
+						<h3 className="testimonial__name">{title}</h3>
+						<p className="testimonial__description">
+							{description}
+						</p>
+					</SwiperSlide>
+				))}
+			</Swiper>
 		</div>
 	);
 };
---
+++ src/component/testimonials/testimonialsData.js
@@ -0,0 +1,51 @@
+import Image1 from "../../assets/testimonial1.png";
+import Image2 from "../../assets/testimonial2.png";
+import Image3 from "../../assets/testimonial3.png";
+import Image4 from "../../assets/testimonial4.png";
+import Image5 from "../../assets/testimonial5.png";
+import Image6 from "../../assets/testimonial6.png";
+
+export const Data = [
+	{
+		id: 1,
+		image: Image1,
+		title: "Jhon Doe",
+		description:
+			"A really good job, all aspects of the project were followed step by step and with good results.",
+	},
+	{
+		id: 2,
+		image: Image2,
+		title: "Harry Clinton",
+		description:
+			"A really good job, all aspects of the project were followed step by step and with good results.",
+	},
+	{
+		id: 3,
+		image: Image3,
+		title: "Sara Cill",
+		description:
+			"A really good job, all aspects of the project were followed step by step and with good results.",
+	},
+	{
+		id: 4,
+		image: Image4,
+		title: "Douglas Winters",
+		description:
+			"A really good job, all aspects of the project were followed step by step and with good results.",
+	},
+	{
+		id: 5,
+		image: Image5,
+		title: "Scott Linsey",
+		description:
+			"A really good job, all aspects of the project were followed step by step and with good results.",
+	},
+	{
+		id: 6,
+		image: Image6,
+		title: "Sara Cill",
+		description:
+			"A really good job, all aspects of the project were followed step by step and with good results.",
+	},
+];
